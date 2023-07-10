97da90cc9e69471c8798c1fbe19ef863a5b0a46e
Siphamandla750 <139158636+Siphamandla750@users.noreply.github.com>
2023-07-10 20:57:07 +0200

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#Request user for their name using the input()
+#Print the name out using the input
+#Request user for their age using the input()
+#Print the user age out
+#Print out Hello World!
+
+name = input("What is your name:")
+   print(name)
+age = (int(input("how old are you:"))
+    print(age)
+print("Hello World")
